dc295876ee5eee0604011b3866b861a2d37222b2
Tin Phan <phanthanhtincr7@gmail.com>
2024-01-17 22:32:26 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<div className="text-red-500 ">hello tinphancr7</div>
+			<div className="text-red-500 ">hello tinphancr788</div>
 		</>
 	);
 }
